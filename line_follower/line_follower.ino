30ce6f9eded2b8f664b5b7bb550f2f92403a4ef1
Jason Van Kerkhoven <jason.vk@outlook.com>
2018-02-19 02:24:14 -0500

    fix motor control

--- line_follower/line_follower.ino
+++ line_follower/line_follower.ino
@@ -1,91 +1,89 @@
 //declaring constants
-int FWD_LEFT = 9;
-int BCK_LEFT = 10;
-int FWD_RIGHT = 6;
-int BCK_RIGHT = 5;
-int DUTY_R = 100;
-int DUTY_L = 150;
+#define LEFT_PWN 10
+#define LEFT_DIR 9
+#define RIGHT_PWN 5
+#define RIGHT_DIR 6
+#define DUTY 120
+
+#define IR_RIGHT 7
+#define IR_LEFT 8
+#define IR_CENTER 11
 
 //init ports as I/O
 void setup()
 {
-  //config serial
-  //Serial.begin(9600);
-  //Serial.print("serial init\n");
+  //config serial for debug
+  Serial.begin(9600);
+  Serial.print("serial init\n");
   
   //set motor GPIO as output
-  pinMode(FWD_LEFT , OUTPUT);
-  pinMode(BCK_LEFT , OUTPUT);
-  pinMode(FWD_RIGHT , OUTPUT);
-  pinMode(BCK_RIGHT , OUTPUT);
+  pinMode(LEFT_PWN , OUTPUT);
+  pinMode(LEFT_DIR , OUTPUT);
+  pinMode(RIGHT_PWN , OUTPUT);
+  pinMode(RIGHT_DIR , OUTPUT);
+
+  //set IR GPIO as inpurt
+  pinMode(IR_RIGHT, INPUT);
+  pinMode(IR_LEFT, INPUT);
+  pinMode(IR_CENTER, INPUT);
 }
 
 
-//print values of IR sensor
-void printSensor(boolean r, boolean l, boolean c)
-{
-  Serial.print("\n\nR: ");
-  Serial.print(r, DEC);
-  Serial.print("\nL: ");
-  Serial.print(l, DEC);
-  Serial.print("\nC: ");
-  Serial.print(c, DEC);
-}
-
-
-
-//operate wheels
+//drive function
 void drive(char dir)
 {
-  analogWrite(BCK_RIGHT, 0);
-  analogWrite(BCK_LEFT,  0);
-  analogWrite(FWD_LEFT,  0);
-  analogWrite(FWD_RIGHT, 0);
   switch(dir)
   {
+    //move forward
     case('f'):
-      analogWrite(FWD_LEFT,  DUTY_L);
-      analogWrite(FWD_RIGHT, DUTY_R);
+      digitalWrite(LEFT_DIR, HIGH);
+      digitalWrite(RIGHT_DIR, HIGH);
+      analogWrite(LEFT_PWN, 255-DUTY);
+      analogWrite(RIGHT_PWN, 255-DUTY);
       break;
-    case('l'):
-      analogWrite(FWD_RIGHT, DUTY_R);
-      analogWrite(BCK_LEFT,  DUTY_L);
-      break;
-    case('r'):
-      analogWrite(BCK_RIGHT, DUTY_R);
-      analogWrite(FWD_LEFT,  DUTY_L);
-      break;
+      
+    //move backwards
     case('b'):
-      analogWrite(BCK_RIGHT, DUTY_R);
-      analogWrite(BCK_LEFT,  DUTY_L);
+      digitalWrite(LEFT_DIR, LOW);
+      digitalWrite(RIGHT_DIR, LOW);
+      analogWrite(LEFT_PWN, DUTY);
+      analogWrite(LEFT_PWN, DUTY);
+      break;
+      
+    //turn right
+    case('r'):
+      digitalWrite(LEFT_DIR, HIGH);
+      digitalWrite(RIGHT_DIR, LOW);
+      analogWrite(LEFT_PWN, 255-DUTY);
+      analogWrite(LEFT_PWN, DUTY);
+      break;
+      
+    //turn left
+    case('l'):
+      digitalWrite(LEFT_DIR, LOW);
+      digitalWrite(RIGHT_DIR, HIGH);
+      analogWrite(LEFT_PWN, DUTY);
+      analogWrite(LEFT_PWN, 255-DUTY);
+      break;
+
+    //stop
+    default:
+      digitalWrite(LEFT_DIR, LOW);
+      digitalWrite(RIGHT_DIR, LOW);
+      analogWrite(LEFT_PWN, 0);
+      analogWrite(LEFT_PWN, 0);
       break;
   }
-}
-
-
-//test drive
-void testDrive()
-{
-  drive('f');
-  delay(5000);
-  drive('r');
-  delay(5000);
-  drive('l');
-  delay(5000);
-  drive('b');
-  delay(5000);
-  drive('h');
-  delay(5000);
 }
 
 
 //main
 void loop()
 {
-  //poll sensors (true for tape, false for no tape)
-  bool tapeRight = (digitalRead(7) == 0) ? false : true;
-  bool tapeLeft = (digitalRead(8) == 0) ? false : true;
-  bool tapeCenter = (digitalRead(11) == 0) ? false : true;
+    //poll sensors (true for tape, false for no tape)
+  bool tapeRight = (digitalRead(IR_RIGHT) == 0) ? false : true;
+  bool tapeLeft = (digitalRead(IR_LEFT) == 0) ? false : true;
+  bool tapeCenter = (digitalRead(IR_CENTER) == 0) ? false : true;
 
   //end reached
   if (tapeLeft && tapeCenter && tapeRight)
